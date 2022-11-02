d04701e6ccea7d14c6acb97fd98d2c5c092506a1
vitorgomes <santosgomesv@gmail.com>
2022-11-02 16:30:19 -0300

    mais recursos

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import tweepy
 from decouple import config
 
-api = tweepy.Client(
+cliente = tweepy.Client(
     consumer_key=config('API_Key'),
     consumer_secret=config('API_Key_Secret'),
     access_token=config('Access_Token'),
     access_token_secret=config('Access_Token_Secret')
 )
 
+
+
 try:
-    # postando Tweet pelo script
-    tweet = api.create_tweet(text="My Second Tweet for Bot !")
-    print(tweet)
-except:
-    print('Algo deu errado')
+    #postando Tweet pelo script
+    response = cliente.create_tweet(text="My Fisrt Tweet for Bot !")
+    print(response)
+    # pesquisando por tweets recentes
+    #query ='python'
+    #tweet = cliente.search_recent_tweets(query=query,user_auth=True , max_results=100)
+    #for i in tweet.data:    
+    #    print(i.id , i.text)
+        
+    
+    #retweet 
+    #retweet = cliente.retweet(tweet_id=1587858741478785029,user_auth=True)
+    #print(retweet)
+    # curtindo tweet 
+    # like = cliente.like(tweet_id=1587858741478785029, user_auth=True)
+
+    # comentando tweet
+    #response = cliente.create_tweet(in_reply_to_tweet_id=1587858741478785029,text='Hello')
+    #print(response)
+
+    # comentando tweets da pesquisa recente
+    #query ='python'
+    #tweet = cliente.search_recent_tweets(query=query,user_auth=True , max_result=100)
+    #for i in tweet.data:    
+    #    print(i.id , i.text)
+    #    response = cliente.create_tweet(in_reply_to_tweet_id=i.id,text='Hello !')
+except tweepy.TweepyException as e:
+    print(e)
